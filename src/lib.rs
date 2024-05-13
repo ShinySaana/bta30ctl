2b3cec0ba0fd5c99a0cd26c88eb6deb5e72b4126
ShinySaana <shinysaana@gmail.com>
2024-05-13 06:15:36 +0200

    task: Add some commands, refactor public API
    There's a better way to encode what kind of request needs to be done, and to not duplicate GET/SET code somewhere but I'm putting this much on CVS for now.

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,6 @@ use std::{collections::BTreeSet, pin::Pin, str::FromStr};
 use btleplug::api::{CharPropFlags, Characteristic, Peripheral, WriteType};
 use uuid::Uuid;
 
-// let led_off_command: [u8; 5] = [0x00, 0x0a, 0x04, 0x3e, 0x00];
-// let led_on_command: [u8; 5] = [0x00, 0x0a, 0x04, 0x3e, 0x01];
-// let boot_mode_on_command: [u8; 5] = [0x00, 0x0a, 0x04, 0x0b, 0x01];
-// let power_off_command: [u8; 5] = [0x00, 0x0a, 0x84, 0x25, 0x00];
-
 lazy_static! {
     static ref CSR_GAIA_SERVICE_UUID: Uuid = Uuid::from_str("00001100-d102-11e1-9b23-00025b00a5a5").unwrap();
     static ref CSR_GAIA_COMMAND_ENDPOINT_UUID: Uuid = Uuid::from_str("00001101-d102-11e1-9b23-00025b00a5a5").unwrap();
@@ -24,15 +19,29 @@ lazy_static! {
     };
 }
 
+#[derive(Copy, Clone)]
+enum CommandKind {
+    Request,
+    Command,
+}
+
+impl Into<WriteType> for CommandKind {
+    fn into(self) -> WriteType {
+        match self {
+            CommandKind::Request => WriteType::WithResponse,
+            CommandKind::Command => WriteType::WithoutResponse,
+        }
+    }
+}
 
 pub struct Command {
+    kind: CommandKind,
     payload: Vec<u8>,
-    write_type: WriteType
 }
 
 pub fn send_command <'a, P> (peripheral: &'a P, command: &'a Command) -> Pin<Box<dyn std::future::Future<Output = Result<(), btleplug::Error>> + Send + 'a>>
     where P: Peripheral + 'a {
-    peripheral.write(&CMD_CHAR, &command.payload, command.write_type)
+    peripheral.write(&CMD_CHAR, &command.payload, command.kind.into())
 }
 
 #[derive(Debug)]
@@ -40,6 +49,8 @@ pub enum CommandBuilderError {
     /// Returned when requested volume is superior to 60
     IncorrectVolumeValue
 }
+
+type CommandResult = Result<Command, CommandBuilderError>;
 
 #[repr(u8)]
 pub enum OperationalMode {
@@ -56,20 +67,59 @@ pub enum VolumeModeSetting {
     HundredPercent = 0x05,
 }
 
-pub fn set_volume_command(volume: u8) -> Result<Command, CommandBuilderError> {
+#[repr(u8)]
+pub enum LedIndicator {
+    Off = 0x00,
+    On = 0x01,
+}
+
+#[repr(u8)]
+pub enum BootOnPowerOn {
+    Off = 0x00,
+    On = 0x01,
+}
+
+pub fn set_volume_command(volume: u8) -> CommandResult {
     if volume > 60 {
         return Err(CommandBuilderError::IncorrectVolumeValue)
     }
 
     Ok(Command {
         payload: vec![0x00, 0x0a, 0x04, 0x02, volume],
-        write_type: WriteType::WithResponse
+        kind: CommandKind::Request
     })
 }
 
-pub fn set_volume_mode_setting(mode: OperationalMode, vms: VolumeModeSetting) -> Result<Command, CommandBuilderError> {
+pub fn set_volume_mode_setting_command(mode: OperationalMode, vms: VolumeModeSetting) -> CommandResult {
     Ok(Command {
         payload: vec![0x00, 0x0a, 0x04, 0x4f, mode as u8, vms as u8],
-        write_type: WriteType::WithResponse
+        kind: CommandKind::Request
     })
+}
+
+pub fn set_led_indicator_command(led_indicator: LedIndicator) -> CommandResult {
+    Ok(
+        Command {
+            payload: vec![0x00, 0x0a, 0x04, 0x3e, led_indicator as u8],
+            kind: CommandKind::Request
+        }
+    )
+}
+
+pub fn set_boot_mode_command(boot_mode: BootOnPowerOn) -> CommandResult {
+    Ok(
+        Command {
+            payload: vec![0x00, 0x0a, 0x04, 0x0b, boot_mode as u8],
+            kind: CommandKind::Request
+        }
+    )
+}
+
+pub fn power_off_commqnd() -> CommandResult {
+    Ok(
+        Command {
+            payload: vec![0x00, 0x0a, 0x84, 0x25, 0x00],
+            kind: CommandKind::Command
+        }
+    )
 }
